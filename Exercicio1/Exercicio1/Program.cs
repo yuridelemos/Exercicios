086c07d5b843a106cfc97b6ca7754d3cbc89070c
Yuri de Lemos <yuridelemos@outlook.com>
2022-03-06 22:04:55 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+            retangulo.Largura = double.Parse(Console.ReadLine());
+            retangulo.Altura = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("AREA = " + retangulo.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRO = " + retangulo.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + retangulo.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+    }
+}
---
+++ Exercicio2/Exercicio2/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Exercicio2
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double Salario;
+        public double Imposto;
+
+
+        public double SalarioLiquido()
+        {
+            return Salario - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            Salario = Salario + (Salario * porcentagem / 100.0);
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", R$ " + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
