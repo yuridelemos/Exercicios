0ce36308739a46762d564b5c366d65ed813b5c87
Yuri de Lemos <yuridelemos@outlook.com>
2022-03-11 20:52:11 -0300

    Exercício de vetores com classe

---
+++ Exercicio7/Exercicio7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            QuartosPousada[] vect = new QuartosPousada[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine("\nAluguel #{0}: ", i+1);
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto:  ");
+                int room = int.Parse(Console.ReadLine());
+                vect[room] = new QuartosPousada(name, email);
+            }
+
+
+            Console.WriteLine("\nQuartos ocupados:");
+            for (int i = 0; i < vect.Length; i++)
+            {
+                if(vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+
+        }
+    }
+}
---
+++ Exercicio7/Exercicio7/QuartosPousada.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Exercicio7
+{
+    class QuartosPousada
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+
+        public QuartosPousada(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return Name + ", " + Email;
+        }
+
+
+    }
+}
