40eebc70e8088a23fd7ce3866560974e2a5049ed
Yuri de Lemos <yuridelemos@outlook.com>
2022-03-16 19:30:08 -0300

    Exercício de matrizes

---
+++ Exercicio9/Exercicio9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercicio9
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int contador = 0;
+
+            int[,] matriz = new int[x, x];
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < x; j++)
+                {
+                    matriz[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.WriteLine("\nMain diagonal: ");
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                 for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    if (i == j)
+                        Console.Write(matriz[i, j] + " ");
+                    if (matriz[i, j] < 0)
+                        contador++;
+                }
+            }
+            Console.WriteLine("\nNegative numbers = " + contador);
+        }
+    }
+}
