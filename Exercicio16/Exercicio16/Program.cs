c2b66da7a55e1006e7be795aa0e9424164a8f2c2
Yuri de Lemos <yuridelemos@outlook.com>
2022-04-04 23:40:23 -0300

    Prática de herança e Polimorfismo

---
+++ Exercicio16/Exercicio16/Program.cs
@@ -0,0 +1,49 @@
+﻿using Exercicio16.Entities;
+using System;
+namespace Exercicio16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+            Console.Write("Enter the number of products: ");
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= count; i++)
+            {
+                Console.WriteLine("Product #1 data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char ProductOrigen = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string Name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine());
+                if (ProductOrigen == 'i' || ProductOrigen == 'I')
+                {
+                    Console.Write("Customs fee: ");
+                    double customs = double.Parse(Console.ReadLine());
+                    products.Add(new ImportedProduct(Name, price, customs));
+                }
+                if(ProductOrigen == 'u' || ProductOrigen == 'U')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    products.Add(new UsedProduct(Name, price, date));
+                }
+                else
+                {
+                    products.Add(new Product(Name, price));
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("PRICE TAGS:");
+                foreach (Product prod in products)
+                {
+                    Console.WriteLine(prod.PriceTag());
+                }
+
+            }
+
+        }
+    }
+}
