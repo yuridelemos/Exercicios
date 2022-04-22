2eebf6e3e771aaf65955c153d8309333f9b5baa3
Yuri de Lemos <yuridelemos@outlook.com>
2022-04-21 22:13:58 -0300

    Tratamento de exceções

---
+++ Exercicio19/Exercicio19/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Exercicio19.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+
+    }
+}
---
+++ Exercicio19/Exercicio19/Entities/Reservation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Exercicio19.Entities.Exceptions;
+
+namespace Exercicio19.Entities
+{
+    class Reservation
+    {
+        public int Room { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int room, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-out date must be after Check-in date");
+            }
+            Room = room;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                throw new DomainException("Reservation dates for update must be future dates.");
+            }
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-out date must be after Check-in date");
+
+            }
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room " + Room + ", check-in: " + CheckIn.ToString("dd/MM/yyyy") + ", check-out: " + CheckOut.ToString("dd/MM/yyyy") + ", " + Duration() + " nights.";
+        }
+    }
+}
